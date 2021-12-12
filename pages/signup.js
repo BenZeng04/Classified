b31302085b6d28c375b0b45aee2d2f5ccfbf3ad2
yeahbennou <38169671+BenZeng04@users.noreply.github.com>
2021-12-12 15:44:57 -0500

    Add anonymous authentication

--- pages/signup.js
+++ pages/signup.js
@@ -3,6 +3,7 @@ import useFirebaseAuth, {FINISHED} from "../utils/firebase/auth";
 import {Divider, Error, Title} from "../utils/components/utils";
 import GlobalHeader from "../utils/components/header";
 import SignupForm from "../utils/components/forms/signupForm";
+import {auth, db} from "../utils/firebase/firebase";
 
 export default function SignUp() {
     const authState = useFirebaseAuth();
@@ -23,6 +24,41 @@ export default function SignUp() {
                 {
                     error == null ? <></> : <Error error={error}/>
                 }
+                <button className={'button'} onClick={(event) => {
+                    event.preventDefault();
+                    auth.signInAnonymously()
+                        .then(() => {
+                            // Signed in..
+                            const unsubscribe = auth.onAuthStateChanged((user) => {
+                                const authStateChanged = async (user) => {
+                                    if (user) {
+                                        // User is signed in, see docs for a list of available properties
+                                        // https://firebase.google.com/docs/reference/js/firebase.User
+                                        const userDoc = db.collection("users").doc(user.uid);
+                                        await userDoc.set({
+                                            username: 'Anonymous User',
+                                            currentMatchID: null
+                                        }).then(() => {
+                                            console.log("Document successfully written!");
+                                            window.location.replace("/")
+                                        })
+                                        .catch((error) => {
+                                            console.error("Error writing document: ", error);
+                                        });
+                                        // ...
+                                    } else {
+                                        // User is signed out
+                                        // ...
+                                        console.log("should never happen");
+                                    }
+                                }
+                                authStateChanged(user).then(() => unsubscribe());
+                            });
+                        }).catch((error) => {
+                        setError(error.message);
+                    })
+
+                }}>Or... Sign Up Anonymously</button>
             </div>
         </div>
 
